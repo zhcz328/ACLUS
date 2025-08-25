fd6050d326a5477f008ea423833eb7eb39c01544
Chunzheng <1724735214@qq.com>
2025-08-25 09:55:34 +0800

    update

--- SAM/utils/loss_functions/nd_softmax.py
+++
@@ -1,5 +0,0 @@
-import torch
-from torch import nn
-import torch.nn.functional as F
-
-softmax_helper = lambda x: F.softmax(x, 1)
--- model/__init__.py
+++
@@ -1,2 +0,0 @@
-#-*- coding:utf-8 -*-
-from .aclus_model import ACLUSModel
